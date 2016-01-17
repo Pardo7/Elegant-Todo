b3b47bcafd6c891ac7f4fece675847d9eb891789
Pardo <wpardo@MB-WPARDO01.local>
2016-01-16 23:16:45 -0800

    Implemented our oncreate logic and our view adapter for displaying list items.'

--- app/src/main/java/com/pardowalter/simpletodo/MainActivity.java
+++ app/src/main/java/com/pardowalter/simpletodo/MainActivity.java
@@ -4,13 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvItems = (ListView)findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
     }
 
     @Override
